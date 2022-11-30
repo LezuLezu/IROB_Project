faea7b5c73636f79d6dcac52c64e2f9512c33815
Zoë <s1107714@student.hsleiden.nl>
2022-11-30 12:41:38 +0100

    Connected sw pro controller, reading buttons goes fine. xbox controller seems to have connection issues, testing  shall continue with sw controller. ~Lezu

---
+++ bluetooth_tracking/BT_controller/bt_controller_map.py
@@ -0,0 +1,16 @@
+#import evdev
+from evdev import InputDevice, categorize, ecodes
+
+#creates object 'gamepad' to store the data
+#you can call it whatever you like
+gamepad = InputDevice('/dev/input/event0')
+
+#prints out device info at start
+print(gamepad)
+
+#evdev takes care of polling the controller in a loop
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_KEY:
+        print(categorize(event))
+    elif event.type == ecodes.EV_ABS:
+        print(categorize(event))
