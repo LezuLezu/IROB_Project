f6454e91b5e97ee520e1811430b0ff580bc713e1
LezuLezu <zoe.vandijk@casema.nl>
2023-01-12 15:23:52 +0100

    attempted different library for SR

--- BaseCodes/translate/SR_Spinx.py
+++ BaseCodes/translate/SR_Spinx.py
@@ -5,24 +5,20 @@ translator = Translator()
 
 r = sr.Recognizer()
 
-# with sr.Microphone() as source:
-#     print("You can speak now")
-#     audio = r.listen(source, timeout=5, phrase_time_limit=5)
-#     print("Time Over")
 pickUp = sr.AudioFile("D:/IROB_Coding/Project/BaseCodes/translate/pick-up-the-phone-1.wav")
 with pickUp as source:
     audio = r.record(source)
 
-#Default Mic as source, it listens
 
 try:
-    # print("TEXT: "+r.recognize_google(audio))
-    text_record = r.recognize_google(audio)
-    #Prints the Output
+    text_record = r.recognize_sphinx(audio)
 
     translatedText = translator.translate(text_record, dest='nl')
+
+    text_record2 = r.recognize_google(audio)
+    translatedText2 = translator.translate(text_record2, dest='nl')
     print(translatedText.text)
 
 except:
+    print("Dindt recognize")
     pass
-    #Does nothing, if error occurred(No error is showed on-scree)
