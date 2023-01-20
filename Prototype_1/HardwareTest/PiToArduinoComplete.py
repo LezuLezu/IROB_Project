a81a44f5610d45fc58121a9d3369849a47f23ae2
Dishango4 <dishang04@gmail.com>
2023-01-20 15:29:35 +0000

    cleaned the code wasnt DRY coded: too many lines

--- Prototype_1/HardwareTest/PiToArduinoComplete.py
+++ Prototype_1/HardwareTest/PiToArduinoComplete.py
@@ -17,45 +17,34 @@ import serial
 #DECLERATIONS
 BUTTON1 = 7
 LED = 11
-
+officalLanguage = "nl"
+r = sr.Recognizer()
 
 def main():
     #SETUPS
-    r = sr.Recognizer()
+    
     GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(LED, GPIO.OUT)
     GPIO.output(LED, GPIO.LOW)
 
     while True:
-        if GPIO.input(BUTTON1) == True:
+        if GPIO.input(BUTTON1) == False:
             with serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=1) as arduino:
                 with sr.Microphone() as source:
                     try:
-                        print("Silence please, calibrating...")
-                        r.adjust_for_ambient_noise(source, duration=2)
-                        print("Say something when the light is on")
-                        # arduino.write(str.encode("Say something when the light is on"))
-                        GPIO.output(LED, GPIO.HIGH)
-                        audio = r.listen(source, timeout=5, phrase_time_limit=20)
-                        GPIO.output(LED, GPIO.LOW)
+                        audio = listenToAudio(source)
                         speech = r.recognize_google(audio)
                         print("You said "+speech+"\n")
 
                         translationText, detectedLang = translateText(speech)
                         arduino.write(str.encode(translationText))      #DUTCH TO ARDUINO
-
-                        r.adjust_for_ambient_noise(source, duration=2)
-                        print("Say something back when the light is on")
-                        # arduino.write(str.encode("Say something back when the light is on"))
-                        GPIO.output(LED, GPIO.HIGH)
-                        audio = r.listen(source, timeout=5, phrase_time_limit=20)
-                        GPIO.output(LED, GPIO.LOW)
-                        speech = r.recognize_google(audio, language="nl")
+                        
+                        audio = listenToAudio(source)
+                        speech = r.recognize_google(audio, language=officalLanguage)
                         print("You said "+speech+"\n")
 
                         originLangText = translateOriginLang(speech, detectedLang)
                         arduino.write(str.encode(originLangText))      #ORIGIN LANGUAGE TO ARDUINO
-
                                   
                     except sr.UnknownValueError:
                         print("Could not understand audio")
@@ -64,10 +53,23 @@ def main():
                         print("Request error; {0}".format(e))
 
 
+#GET THE SPEECH AUDIO FROM USER
+def listenToAudio(source):
+    print("Silence please, calibrating...")
+    r.adjust_for_ambient_noise(source, duration=2)
+    print("Say something when the light is on")
+    # arduino.write(str.encode("Say something when the light is on"))
+    # sleep(2.5)
+    GPIO.output(LED, GPIO.HIGH)
+    audio = r.listen(source, timeout=5, phrase_time_limit=20)
+    GPIO.output(LED, GPIO.LOW)
+    return audio
+    
+
 #TRANSLATE TEXT TO DUTCH
 def translateText(speech):    
     translator = Translator()
-    translationText = translator.translate(speech, "dutch")
+    translationText = translator.translate(speech, officalLanguage)
     translationText2 = translationText.new_text
     print(translationText.new_text)
     detectedLang = detect(speech)
